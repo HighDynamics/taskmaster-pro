54fc6d3fe53341945fb862509459822ca009f5b6
Daniel Denton <danielryandenton@gmail.com>
2021-10-12 12:42:04 -0500

    use jqueryui and touchpunch to implement drag and drop sorting and deleting

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,27 +90,88 @@ $(".list-group").on("click", "span", function () {
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']",
-  function () {
-    // get current text
-    var date = $(this).val().trim();
+$(".list-group").on("blur", "input[type='text']", function () {
+  // get current text
+  var date = $(this).val().trim();
 
-    // get the parent ul's id attribute
-    var status = $(this).closest(".list-group").attr("id").replace("list-", "");
+  // get the parent ul's id attribute
+  var status = $(this).closest(".list-group").attr("id").replace("list-", "");
 
-    // get the task's position in the list of other li elements
-    var index = $(this).closest(".list-group-item").index();
+  // get the task's position in the list of other li elements
+  var index = $(this).closest(".list-group-item").index();
 
-    // update task in array and re-save to localStorage
-    tasks[status][index].date = date;
-    saveTasks();
+  // update task in array and re-save to localStorage
+  tasks[status][index].date = date;
+  saveTasks();
 
-    // recreate span element with bootstrap classes
-    var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(date);
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
 
-    // replace input with span element
-    $(this).replaceWith(taskSpan)
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
 });
+
+// add sorting drag and drop functionality
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    console.log("activate", this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function (event) {
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  update: function (event) {
+    // array to store the task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this)
+      .children()
+      .each(function () {
+        var text = $(this).find("p").text().trim();
+        var date = $(this).find("span").text().trim();
+
+        // add text data to the temp array as an object
+        tempArr.push({
+          text: text,
+          date: date,
+        });
+      });
+    // trim down lists's ID to match object property
+    var arrName = $(this).attr("id").replace("list-", "")
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr
+    saveTasks()
+  },
+});
+
+// list item was dropped in trash
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui){
+    ui.draggable.remove();
+    console.log("drop")
+  },
+  over: function(event, ui){
+    console.log("over")
+  },
+  out: function(event, ui){
+    console.log("out")
+  }
+})
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function () {
